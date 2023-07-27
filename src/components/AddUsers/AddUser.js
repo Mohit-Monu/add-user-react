d90adb39c978b1443f38d845c431e1caab844dc7
mohitr Agarwal <bmohit700@gmail.com>
2023-07-27 14:10:19 +0530

    Added Error Handling

--- src/components/AddUsers/AddUser.js
+++ src/components/AddUsers/AddUser.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./AddUser.css";
+import ErrorModel from "../ErrorModel/ErrorModel";
 
 function AddUser(props) {
   const [username, setUserName] = useState("");
   const [userage, setUserAge] = useState("");
-
+  const [error,setError]=useState()
   function usernameHandler(e) {
     setUserName(e.target.value);
   }
@@ -12,33 +13,47 @@ function AddUser(props) {
     setUserAge(e.target.value);
   }
   function AddUserHandler(e) {
-    e.preventDefault()
-    if(username.trim().length===0 || userage.trim().length===0){
-        return
+    e.preventDefault();
+    if (username.trim().length === 0 || userage.trim().length === 0) {
+      setError({
+        title:"Invalid Input",
+        message:"Please enter name and age"
+      })
+      return;
     }
-    if(+userage<1){
-        return
+    if (+userage < 1) {
+      setError({
+        title:"Invalid age",
+        message:"Age cannot be negative"
+      })
+      return;
     }
-    const obj={
-        username:username,
-        age:userage
-    }
-    props.addUser(obj)
-    setUserName("")
-    setUserAge("")
+    const obj = {
+      username: username,
+      age: userage,
+    };
+    props.addUser(obj);
+    setUserName("");
+    setUserAge("");
+  }
+  function DismisBoxHandler(){
+    setError(null)
   }
   return (
-    <form onSubmit={AddUserHandler}>
-      <div className="form-control">
-        <label> Enter User Name</label>
-        <input onChange={usernameHandler} type="text" value={username} />
-        <label> Enter Age</label>
-        <input onChange={userageHandler} type="number" value={userage} />
-      </div>
-      <button className="button" type="submit">
-        Add User
-      </button>
-    </form>
+    <div>
+      {error && <ErrorModel title={error.title} message={error.message} onDismisBox={DismisBoxHandler} ></ErrorModel>}
+      <form onSubmit={AddUserHandler}>
+        <div className="form-control">
+          <label> Enter User Name</label>
+          <input onChange={usernameHandler} type="text" value={username} />
+          <label> Enter Age</label>
+          <input onChange={userageHandler} type="number" value={userage} />
+        </div>
+        <button className="button" type="submit">
+          Add User
+        </button>
+      </form>
+    </div>
   );
 }
 export default AddUser;
---
+++ src/components/ErrorModel/ErrorModel.js
@@ -0,0 +1,23 @@
+import "./ErrorModel.css";
+import "../AddUsers/AddUser.css";
+
+function ErrorModel(props) {
+  return (
+    <div className="backdrop" >
+    <div className="backdrop" onClick={props.onDismisBox}/>
+
+      <div className="modal">
+        <header className="header">
+          <h2>{props.title}</h2>
+        </header>
+        <div className="content">
+          <p>{props.message}</p>
+          <footer className="actions">
+            <button className="button" onClick={props.onDismisBox} >Okay</button>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ErrorModel;
